4fed05badbc11950abb594009f21004b1f529c19
lSNOTNULL <iream1@naver.com>
2025-04-24 10:38:00 +0900

    기초

---
+++ src/main/java/org/example/bootjwtmaxex/BootJwtMaxExApplication.java
@@ -0,0 +1,13 @@
+package org.example.bootjwtmaxex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJwtMaxExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootJwtMaxExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bootjwtmaxex/BootJwtMaxExApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bootjwtmaxex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJwtMaxExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
